da33948951199e36c892bd734ba014cc7c08b82a
Saba Machavariani <empirre60@gmail.com>
2026-02-11 19:51:16 -0500

    Update Picture.java
    Edits to Author and Version

--- Picture.java
+++ Picture.java
@@ -6,8 +6,8 @@
  *
  * This class was written as an early example for teaching Java with BlueJ.
  * 
- * @author  Michael Kšlling and David J. Barnes
- * @version 2016.02.29
+ * @author  Saba Machavariani
+ * @version 2026.02.11
  */
 public class Picture
 {
